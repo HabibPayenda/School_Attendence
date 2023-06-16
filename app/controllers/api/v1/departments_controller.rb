a8d9983dfa8b3b02a31f18b9be1b4e7144e34b36
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 18:55:48 +0430

    Adds actions to departments_controller

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -3,6 +3,31 @@
 module Api
   module V1
     class DepartmentsController < ApplicationController
+      def index
+        result = Department.all
+        render json: {status: 'success', departments: result}
+      end
+
+      def create
+        result = Department.new(department_params)
+        render json: { status: 'success', department: result } if result.save
+      end
+
+      def update
+        result = Department.find(params[:id]).update(department_params)
+        render json: { status: 'success', department: result } if result.save
+      end
+
+      def destroy
+        result = Department.find(params[:id])
+        render json: { status: 'success', department: result } if result.destroy
+      end
+
+      private
+
+      def department_params
+        params.require(:department).permit(:name, :teacher_id)
+      end
     end
   end
 end
