b8ee862abb65ea6fd02dceab2e12de0b4242128a
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 19:05:10 +0430

    Adds actions to teachers_controller

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -3,6 +3,31 @@
 module Api
   module V1
     class TeachersController < ApplicationController
+      def index
+        result = Teacher.all
+        render json: {status: 'success', teachers: result}
+      end
+
+      def create
+        result = Teacher.new(teacher_params)
+        render json: { status: 'success', teacher: result } if result.save
+      end
+
+      def update
+        result = Teacher.find(params[:id]).update(teacher_params)
+        render json: { status: 'success', teacher: result } if result.save
+      end
+
+      def destroy
+        result = Teacher.find(params[:id])
+        render json: { status: 'success', teacher: result } if result.destroy
+      end
+
+      private
+
+      def teacher_params
+        params.require(:teacher).permit(:name, :email, :phone, :subject, :password)
+      end
     end
   end
 end
