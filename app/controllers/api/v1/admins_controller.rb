a6a7278657b503ae3505bdede040dd516410789c
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 16:59:56 +0430

    Adds create action to admins controller

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -3,6 +3,14 @@
 module Api
   module V1
     class AdminsController < ApplicationController
+      def create
+        result = Admin.new(admin_params)
+        render json: {status: "success", admin: result} if result.save
+      end
+      private
+      def admin_params
+        params.require(:admin).permit(:name, :password, :email, :phone_number, :status)
+      end
     end
   end
 end
