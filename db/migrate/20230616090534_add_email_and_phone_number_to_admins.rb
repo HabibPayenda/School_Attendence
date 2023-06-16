aa55240c337042b068ea146ab6563bfb2801adf6
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 13:36:17 +0430

    Adds email and phone number to admins table

---
+++ db/migrate/20230616090534_add_email_and_phone_number_to_admins.rb
@@ -0,0 +1,6 @@
+class AddEmailAndPhoneNumberToAdmins < ActiveRecord::Migration[7.0]
+  def change
+    add_column :admins, :email, :string
+    add_column :admins, :phone_number, :string
+  end
+end
