b2f749872c5378171c2e18233c82b085cd272264
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 18:59:22 +0430

    Adds actions to parents_controller

--- app/controllers/api/v1/parents_controller.rb
+++ app/controllers/api/v1/parents_controller.rb
@@ -3,6 +3,31 @@
 module Api
   module V1
     class ParentsController < ApplicationController
+      def index
+        result = Parent.all
+        render json: {status: 'success', parents: result}
+      end
+
+      def create
+        result = Parent.new(parent_params)
+        render json: { status: 'success', parent: result } if result.save
+      end
+
+      def update
+        result = Parent.find(params[:id]).update(parent_params)
+        render json: { status: 'success', parent: result } if result.save
+      end
+
+      def destroy
+        result = Parent.find(params[:id])
+        render json: { status: 'success', parent: result } if result.destroy
+      end
+
+      private
+
+      def parent_params
+        params.require(:parent).permit(:student_id, :email, :password, :phone, :name)
+      end
     end
   end
 end
