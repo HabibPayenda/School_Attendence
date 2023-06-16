e3a63c338fea2a1ec68f0392aa0a92f5823dd499
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 19:02:33 +0430

    Adds actions to school classes

--- app/controllers/api/v1/school_classes_controller.rb
+++ app/controllers/api/v1/school_classes_controller.rb
@@ -3,6 +3,31 @@
 module Api
   module V1
     class SchoolClassesController < ApplicationController
+      def index
+        result = SchoolClass.all
+        render json: {status: 'success', classes: result}
+      end
+
+      def create
+        result = SchoolClass.new(classes_params)
+        render json: { status: 'success', class: result } if result.save
+      end
+
+      def update
+        result = SchoolClass.find(params[:id]).update(classes_params)
+        render json: { status: 'success', class: result } if result.save
+      end
+
+      def destroy
+        result = SchoolClass.find(params[:id])
+        render json: { status: 'success', class: result } if result.destroy
+      end
+
+      private
+
+      def classes_params
+        params.require(:school_class).permit(:name, :teacher_id, :room_number)
+      end
     end
   end
 end
