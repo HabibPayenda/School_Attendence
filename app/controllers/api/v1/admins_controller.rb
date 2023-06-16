1c493345be4e54b0feaf889634c6717aacf4f03c
Habib <habiburahmanpayenda@gmail.com>
2023-06-16 18:51:47 +0430

    Adds index update and destroy actions to admins_controller

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -3,9 +3,25 @@
 module Api
   module V1
     class AdminsController < ApplicationController
+
+      def index
+        result = Admin.all
+        render json: {status: 'success', admins: result}
+      end
+
       def create
         result = Admin.new(admin_params)
         render json: { status: 'success', admin: result } if result.save
+      end
+
+      def update
+        result = Admin.find(params[:id]).update(admin_params)
+        render json: { status: 'success', admin: result } if result.save
+      end
+
+      def destroy
+        result = Admin.find(params[:id])
+        render json: { status: 'success', admin: result } if result.destroy
       end
 
       private
