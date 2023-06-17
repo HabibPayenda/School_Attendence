d2c56ed7c2cb9e04d58f12a62173b80bdca22edc
Habib <habiburahmanpayenda@gmail.com>
2023-06-17 14:25:47 +0430

    Adds students_controller

---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class StudentsController < ApplicationController
+      def index
+        result = Student.all
+        render json: { status: 'success', students: result }
+      end
+
+      def create
+        result = Student.new(student_params)
+        render json: { status: 'success', student: result } if result.save
+      end
+
+      def update
+        result = Student.find(params[:id]).update(student_params)
+        render json: { status: 'success', student: result } if result.save
+      end
+
+      def destroy
+        result = Student.find(params[:id])
+        render json: { status: 'success', student: result } if result.destroy
+      end
+
+      private
+
+      def student_params
+        params.require(:student).permit(:name, :email, :password, :phone, :address, :grade, :date_of_birth, :class_id)
+      end
+    end
+  end
+end
+
