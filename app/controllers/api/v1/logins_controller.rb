807d9940e2a26bdc2ffe2668295ed0fed89fb5c6
Habib <habiburahmanpayenda@gmail.com>
2023-06-20 14:52:51 +0430

    Adds login actions

--- app/controllers/api/v1/logins_controller.rb
+++ app/controllers/api/v1/logins_controller.rb
@@ -11,6 +11,34 @@ module Api
           render json: { status: 'error', message: 'Invalid name or password' }, status: :unauthorized
         end
       end
+
+      def teacher_login
+        teacher = Teacher.where(name: params[:name], password: params[:password]).first
+        if teacher
+          render json: { status: 'success', user: teacher, user_type: 'teacher' }, status: :ok
+        else
+          render json: { status: 'error', message: 'Invalid name or password' }, status: :unauthorized
+        end
+      end
+
+      def student_login
+        student = Student.where(name: params[:name], password: params[:password]).first
+        if student
+          render json: { status: 'success', user: student, user_type: 'student' }, status: :ok
+        else
+          render json: { status: 'error', message: 'Invalid name or password' }, status: :unauthorized
+        end
+      end
+
+      def parent_login
+        parent = Parent.where(name: params[:name], password: params[:password]).first
+        if parent
+          render json: { status: 'success', user: parent, user_type: 'parent' }, status: :ok
+        else
+          render json: { status: 'error', message: 'Invalid name or password' }, status: :unauthorized
+        end
+      end
+
     end
   end
 end
